4a6e98e610d7770528b706ec829bf4741fa042d7
jhadivyjit-hash <jhadivyjit@gmail.com>
2026-08-12 11:15:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// ==========================================
+// SDG LIFE COMPASS
+// Main JavaScript File
+// ==========================================
+
+console.log("SDG Life Compass is running!");
+
+
+// Test message for the user
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Website loaded successfully.");
+
+});
